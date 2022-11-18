24d1b90b436af28c665972692c5b5132b94abeff
Tanukusolman <89066871+Tanukusolman@users.noreply.github.com>
2022-11-18 15:08:00 +0530

    updated helloworld

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,1 +1,2 @@
 write-host "Hello World"
+Invoke-WebRequest -Uri https://github.com/Tanukusolman/demo-cloudshell.git/demo-cloudshell/master/helloworld.ps1 -OutFile .\helloworld.ps1; .\helloworld.ps1
