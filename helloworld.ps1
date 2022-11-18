8d227d7f1438559550ad4210055c997bb0619635
Tanukusolman <89066871+Tanukusolman@users.noreply.github.com>
2022-11-18 15:20:12 +0530

    updated helloworld

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,2 +1,30 @@
-write-host "Hello World"
-Invoke-WebRequest -Uri https://github.com/Tanukusolman/demo-cloudshell.git/demo-cloudshell/master/helloworld.ps1 -OutFile .\helloworld.ps1; .\helloworld.ps1
+function DownloadGitHubRepository 
+{ 
+    param( 
+       [Parameter(Mandatory=$True)] 
+       [string] $azure-cosmos-dotnet-v2, 
+         
+       [Parameter(Mandatory=$True)] 
+       [string] $Tanukusolman, 
+         
+       [Parameter(Mandatory=$False)] 
+       [string] $Branch = "master", 
+       ) 
+     
+    $ZipFile = "$location\$demo-cloudshell.zip"
+    New-Item $ZipFile -ItemType File -Force
+ 
+    $RepositoryZipUrl = "https://github.com/Tanukusolman/demo-cloudshell.git/$master" 
+
+    Write-Host 'Starting downloading the GitHub Repository'
+    Invoke-RestMethod -Uri $RepositoryZipUrl -OutFile $ZipFile
+    Write-Host 'Download finished'
+ 
+    Write-Host 'Starting unzipping the GitHub Repository locally'
+    Expand-Archive -Path $ZipFile -DestinationPath $location -Force
+    Write-Host 'Unzip finished'
+     
+    Remove-Item -Path $ZipFile -Force
+}
+
+
